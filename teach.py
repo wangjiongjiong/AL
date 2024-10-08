a45dcc1f98efa6f829e2475d3b5faf9cd1f9a518
root <root@LAPTOP-RC8U93BJ>
2024-10-08 15:08:30 +0800

    py study

--- teach.py
+++ teach.py
@@ -1,7 +1,7 @@
-def whileHanShu(num):
-    if num == 1:
-        return 1
-    if num != 1:
-        return num + whileHanShu(num -1)
+列表名 = [1,2, 3,4,5,6,7,8,9,10]
 
-print(whileHanShu(5))
+def 函数名(x): 
+    return x%2==0
+结果 = filter(函数名,列表名)
+print(结果)
+print(list(结果))
