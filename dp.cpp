5b5189863a889c53fe1ab3aaeb32e192d434517e
wangjiongjiong <1192762465@qq.com>
2024-11-20 10:32:15 +0800

    dp restart

--- dp.cpp
+++ dp.cpp
@@ -970,6 +970,35 @@ public:
     }
 };
 
+class leetcode718
+{
+public:
+    int findLength(vector<int> &nums1, vector<int> &nums2)
+    {
+        // 最长重复子数组的长度
+        // dp数组的定义
+        // dp[i][j] 表示 nums1 i-1 结尾和nums2 j-1结尾的最长重复子数组
+        vector<vector<int>> dp(nums1.size() + 1, vector(nums2.size() + 1, 0));
+        int ans = 0;
+        for (int i = 1; i <= nums1.size(); ++i)
+        {
+            for (int j = 1; j <= nums2.size(); ++j)
+            {
+                if (nums1[i - 1] == nums2[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                if (ans < dp[i][j])
+                {
+                    ans = dp[i][j];
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
 int main()
 {
 }
