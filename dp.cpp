ffa1d8d78cf9ded555c2a92e7dc4f6835fcca369
wangjiongjiong <1192762465@qq.com>
2024-11-20 21:48:42 +0800

    最长公共子序列

--- dp.cpp
+++ dp.cpp
@@ -999,6 +999,35 @@ public:
     }
 };
 
+class leetcode1143
+{
+public:
+    int longestCommonSubsequence(string text1, string text2)
+    {
+        // 最长公共子序列
+        //  这个和最长重复数组有点类似
+        int size1 = text1.size();
+        int size2 = text2.size();
+        vector<vector<int>> dp(size1 + 1, vector<int>(size2 + 1, 0));
+        for (int i = 1; i <= size1; ++i)
+        {
+            for (int j = 1; j <= size2; ++j)
+            {
+                if (text1[i - 1] == text2[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+
+        return dp[size1][size2];
+    }
+};
+
 int main()
 {
 }
