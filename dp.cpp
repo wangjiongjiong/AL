2e7092539e385e25509484c64bea75c9e72384ce
wangjiongjiong <1192762465@qq.com>
2024-11-21 16:53:46 +0800

    子序列问题

--- dp.cpp
+++ dp.cpp
@@ -1028,6 +1028,60 @@ public:
     }
 };
 
+class leetcode1035
+{
+public:
+    int maxUncrossedLines(vector<int> &nums1, vector<int> &nums2)
+    {
+        // 与最长公共子序列一致
+        int size1 = nums1.size();
+        int size2 = nums2.size();
+        vector<vector<int>> dp(size1 + 1, vector<int>(size2 + 1, 0));
+
+        for (int i = 1; i <= size1; ++i)
+        {
+            for (int j = 1; j <= size2; ++j)
+            {
+                if (nums1[i - 1] == nums2[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[size1][size2];
+    }
+};
+
+class leetcode53
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        // 最大数组和
+        // 两种状态，一种状态就是当前nums[i]>dp[i-1] + nums[i]
+        // 一种状态就是 nums[i]<dp[i-1] + nums[i]
+        // 取两种状态最大值就行
+        if (nums.size() == 1)
+        {
+            return nums[0];
+        }
+
+        vector<int> dp(nums.size(), 0);
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.size(); ++i)
+        {
+            dp[i] = max(nums[i], dp[i - 1] + nums[i]);
+        }
+
+        return *max_element(dp.begin(), dp.end());
+    }
+};
+
 int main()
 {
 }
