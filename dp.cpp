76daa4cce70c243382b61c97935ccd0dd342b6ab
wangjiongjiong <1192762465@qq.com>
2024-11-26 16:04:53 +0800

    编辑距离

--- dp.cpp
+++ dp.cpp
@@ -1203,6 +1203,46 @@ public:
     }
 };
 
+class leetcode72
+{
+public:
+    int minDistance(string word1, string word2)
+    {
+        // 编辑距离
+        int word1_size = word1.size();
+        int word2_size = word2.size();
+
+        vector<vector<int>> dp(word1_size + 1, vector<int>(word2_size + 1, 0));
+
+        for (int i = 1; i < word1_size + 1; ++i)
+        {
+            dp[i][0] = i;
+        }
+
+        for (int i = 1; i < word1_size + 1; ++i)
+        {
+            dp[0][i] = i;
+        }
+
+        for (int i = 1; i < word1_size + 1; ++i)
+        {
+            for (int j = 1; j < word2_size + 1; ++j)
+            {
+                if (word1[i - 1] == word2[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = min(dp[i - 1][j - 1] + 1, min(dp[i - 1][j] + 1, dp[i][j - 1] + 1));
+                }
+            }
+        }
+
+        return dp[word1_size][word2_size];
+    }
+};
+
 int main()
 {
 }
